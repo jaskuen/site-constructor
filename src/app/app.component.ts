67149481df8f12ff228ae35b509d33ee0b2fd5d1
jaskuen <100339401+jaskuen@users.noreply.github.com>
2024-07-19 22:48:50 +0300

    First commit. Some blocks and components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from "./blocks/header/header.component";
+import {TitleComponent} from "./blocks/title/title.component";
+import {TabsComponent} from "./components/tabs/tabs.component";
+import {SiteCreatorComponent} from "./blocks/site-creator/site-creator.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, TitleComponent, TabsComponent, SiteCreatorComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'untitled1';
+  title = 'MyFirstAngularProject';
 }
